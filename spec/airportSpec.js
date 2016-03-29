9d45eaa51765c3aa997a9aa2d48c20d90d7e5108
Kyle Buttner <kylebuttner@gmail.com>
2016-03-29 15:33:10 +0100

    added land spy

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,23 +1,34 @@
 describe("Airport", function() {
 
+  var plane, airport;
+
   beforeEach(function() {
-    var plane = jasmine.createSpyObj("plane", ["land"]);
-    var airport = new Airport();
+    airport = new Airport();
   });
 
   describe("When weather is not stormy", function() {
     beforeEach(function() {
+      airport = {
+        isStormy: false
+      };
+      plane = {
+        land: 'this does not matter'
+      };
+      landSpy = spyOn(plane, "land");
       // var weather = jasmine.createSpyObj("weather", ["isStormy"]);
       // weather.andCallFake(function() {return false;})
-      var weather = jasmine.createSpy("weather");
+      // var weather = jasmine.createSpy("weather");
       // weather.isStormy.andReturn(false);
-      spyOn(weather, "isStormy").and.callFake(function() { return false; })
+      // spyOn(weather, "isStormy").and.callFake(function() { return false; })
     });
 
     describe("Landing planes", function() {
       it("should add planes after landing", function() {
-        var plane = jasmine.createSpyObj("plane", ["isLanded"]);
-        plane.isLanded.andReturn(false);
+        plane = {
+          isLanded: false,
+        };
+        expect(landSpy).toHaveBeenCalled();
+        // plane.isLanded.andReturn(false);
         airport.accept(plane);
         expect(airport.planes).toContain(plane);
       });
